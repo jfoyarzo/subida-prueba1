2d0812040e4dc61f7bb6aaa0dccee5976ad9676c
jfoyarzo <foyarzo621@gmail.com>
2022-03-11 01:19:47 -0300

    Version 1 - Agregado smooth scroll mediante js, cambio en scroll behavior en CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+
     $('[data-toggle="tooltip"]').tooltip()
+
+    $('a').click(function () {
+        var gato = this.hash
+
+        $('html, body').animate(
+            {
+                scrollTop: $(gato).offset().top -20
+            },
+            1000
+        )
+    })
+
+
 })
