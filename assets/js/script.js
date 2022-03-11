16d10212a6a1c672e47826f9071613c8c10bf663
jfoyarzo <foyarzo621@gmail.com>
2022-03-11 04:27:46 -0300

    Version 1 - Se agrega estilos CSS a elementos <hr>, se documenta código mediante comentarios.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
+
+//Inicio de script con (document).ready para que el script se ejecute luego de que se hayan cargado los elementos del DOM
+
 $(document).ready(function () {
 
-
+    //Declaración para inicializar los tooltips de los íconos de RRSS en el Footer
     $('[data-toggle="tooltip"]').tooltip()
 
+    //Smooth scroll con velocidad personalizada para cuando se hace click en los links de la navbar que llevan a las distintas secciones de la página
     $('a').click(function () {
         var gato = this.hash
 
         $('html, body').animate(
             {
-                scrollTop: $(gato).offset().top -20
+                scrollTop: $(gato).offset().top - 20
             },
             1000
         )
